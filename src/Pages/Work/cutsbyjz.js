4298436ba6e8ea4931fc2c44d3f8b20035c25a7c
Branden Phan <branden.phan@gmail.com>
2023-01-08 22:37:26 -0500

    added darkmode to cutsbyjz panel

--- src/Pages/Work/cutsbyjz.js
+++ src/Pages/Work/cutsbyjz.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, IconButton, Typography } from "@mui/material";
 import BackNav from "../../Components/BackNav";
 import { useWidth } from "../../Context/WidthContext";
+import { useTheme } from "../../Context/ThemeContext";
 import cutsbyjz1 from "../../Images/Work/cutsbyjz/cutsbyjz1.png";
 import cutsbyjz2 from "../../Images/Work/cutsbyjz/cutsbyjz2.png";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
@@ -21,6 +22,7 @@ import mobi6 from "../../Images/Work/cutsbyjz/mobi6.png";
 
 const Cutsbyjz = () => {
     const { width } = useWidth();
+    const { theme } = useTheme();
 
     const [fullImageNumber, setFullImageNumber] = React.useState(0);
     const nextFullImage = () => {
@@ -63,7 +65,7 @@ const Cutsbyjz = () => {
     }, []);
 
     return (
-        <div style={{position: "absolute", top: "0", left: "0", width: "100%", overflow: "hidden", backgroundColor: "rgb(237, 242, 251, 0.45)"}}>
+        <div style={{position: "absolute", top: "0", left: "0", width: "100%", overflow: "hidden", backgroundColor: theme === "light" ? "rgb(237, 242, 251, 0.45)" : "#2D2D2D"}}>
             <div style={{backgroundColor: "#DADADA", display: "flex", justifyContent: "center"}}>
                 {width > 1000 ?
                     <img src={cutsbyjz1} alt="CutsbyjzLargeLandingImage" height="500px" width="auto" />
@@ -73,26 +75,26 @@ const Cutsbyjz = () => {
             </div>
 
             <Grid container sx={{marginLeft: width > 1400 ? "150px" : width > 1100 ? "120px" : width > 900 ? "100px" : width > 700 ? "70px" : "40px", marginTop: width > 700 ? "100px" : "80px", display: "flex", flexDirection: "column"}}>
-                <div style={{border: "1px solid black", marginBottom: "20px", width: "60px"}} />
-                <Typography variant="h5" sx={{fontFamily: "Nunito", fontWeight: "bold"}}>
+                <div style={{border: theme === "light" ? "1px solid black" : "1px solid white", transition: "0.3s", marginBottom: "20px", width: "60px"}} />
+                <Typography variant="h5" sx={{fontFamily: "Nunito", fontWeight: "bold", color: theme === "light" ? "black" : "white", transition: "0.3s"}}>
                     Cutsbyjz
                 </Typography>
-                <Typography variant="subtitle1" align="left" sx={{fontFamily: "Nunito", marginTop: "10px"}}>Front-end Developer</Typography>
+                <Typography variant="subtitle1" align="left" sx={{fontFamily: "Nunito", marginTop: "10px", color: theme === "light" ? "black" : "white", transition: "0.3s"}}>Front-end Developer</Typography>
 
-                <Typography variant="subtitle1" align="left" sx={{fontFamily: "Nunito", marginTop: "10px"}}>Work in progress!</Typography>
+                <Typography variant="subtitle1" align="left" sx={{fontFamily: "Nunito", marginTop: "10px", color: theme === "light" ? "black" : "white", transition: "0.3s"}}>Work in progress!</Typography>
             </Grid>
 
             <Grid container sx={{marginLeft: width > 1400 ? "150px" : width > 1100 ? "120px" : width > 900 ? "100px" : width > 700 ? "70px" : "40px", marginTop: "50px", display: "flex", flexDirection: "column", width: width > 1800 ? "50%" : width > 900 ? "60%" : "80%"}}>
-                <Typography variant="h6" sx={{fontFamily: "Nunito", fontWeight: "bold"}}>
+                <Typography variant="h6" sx={{fontFamily: "Nunito", fontWeight: "bold", color: theme === "light" ? "black" : "white", transition: "0.3s"}}>
                     Description
                 </Typography>
-                <Typography variant="subtitle1" align="left" sx={{fontFamily: "Nunito", marginTop: "10px", marginLeft: "25px"}}>• Developing a full-stack web-application using React TypeScript, designing a modern and responsive user interface to showcase various information regarding the barbershop</Typography>
-                <Typography variant="subtitle1" align="left" sx={{fontFamily: "Nunito", marginTop: "10px", marginLeft: "25px"}}>• Working in a team of two, utilizing Git tools such as issues, sprint boards, and pull requests while following AGILE methodologies</Typography>
-                <Typography variant="subtitle1" align="left" sx={{fontFamily: "Nunito", marginTop: "10px", marginLeft: "25px"}}>• Throughly testing all code to ensure the web-application is supported by major web browsers and bug free</Typography>
+                <Typography variant="subtitle1" align="left" sx={{fontFamily: "Nunito", marginTop: "10px", marginLeft: "25px", color: theme === "light" ? "black" : "white", transition: "0.3s"}}>• Developing a full-stack web-application using React TypeScript, designing a modern and responsive user interface to showcase various information regarding the barbershop</Typography>
+                <Typography variant="subtitle1" align="left" sx={{fontFamily: "Nunito", marginTop: "10px", marginLeft: "25px", color: theme === "light" ? "black" : "white", transition: "0.3s"}}>• Working in a team of two, utilizing Git tools such as issues, sprint boards, and pull requests while following AGILE methodologies</Typography>
+                <Typography variant="subtitle1" align="left" sx={{fontFamily: "Nunito", marginTop: "10px", marginLeft: "25px", color: theme === "light" ? "black" : "white", transition: "0.3s"}}>• Throughly testing all code to ensure the web-application is supported by major web browsers and bug free</Typography>
             </Grid>
 
             <Grid container sx={{marginLeft: width > 1400 ? "150px" : width > 1100 ? "120px" : width > 900 ? "100px" : width > 700 ? "70px" : "40px", marginTop: "50px", display: "flex", flexDirection: "column", width: width > 1800 ? "50%" : width > 900 ? "60%" : "80%", marginBottom: width > 700 ? "90px" : "60px"}}>
-                <Typography variant="h6" sx={{fontFamily: "Nunito", fontWeight: "bold"}}>
+                <Typography variant="h6" sx={{fontFamily: "Nunito", fontWeight: "bold", color: theme === "light" ? "black" : "white", transition: "0.3s"}}>
                     Technologies
                 </Typography>
                 <Grid container sx={{display: "flex", flexDirection: "row", justifyContent: "flex-start", marginTop: "20px"}}>
@@ -105,18 +107,18 @@ const Cutsbyjz = () => {
             </Grid>
 
             <Grid container sx={{marginLeft: width > 1400 ? "150px" : width > 1100 ? "120px" : width > 900 ? "100px" : width > 700 ? "70px" : "40px", marginRight: width > 1400 ? "150px" : width > 1100 ? "120px" : width > 900 ? "100px" : width > 700 ? "70px" : "40px", marginTop: "50px", display: "flex", flexDirection: "column", width: width > 1800 ? "50%" : width > 900 ? "60%" : "80%", marginBottom: width > 700 ? "90px" : "60px"}}>
-                <div style={{border: "1px solid black", marginBottom: "20px", width: "60px"}} />
-                <Typography variant="h6" sx={{fontFamily: "Nunito", fontWeight: "bold"}}>
+                <div style={{border: theme === "light" ? "1px solid black" : "1px solid white", marginBottom: "20px", width: "60px"}} />
+                <Typography variant="h6" sx={{fontFamily: "Nunito", fontWeight: "bold", color: theme === "light" ? "black" : "white", transition: "0.3s"}}>
                     Full Screen Design - {fullImageNumber + 1}/6
                 </Typography>
                 {width > 800 ? 
                     <div style={{marginTop: "20px", display: "flex", flexDirection: "row", alignItems: "center"}}>
                         <IconButton onClick={() => {prevFullImage()}}>
-                            <IoIosArrowBack style={{fontSize: width > 1000 ? "2rem" : "1.3rem", color: "black"}} />
+                            <IoIosArrowBack style={{fontSize: width > 1000 ? "2rem" : "1.3rem", color: theme === "light" ? "black" : "white", transition: "0.3s"}} />
                         </IconButton>
                         <img src={fullImageNumber === 0 ? full1 : fullImageNumber === 1 ? full2 : fullImageNumber === 2 ? full3 : fullImageNumber === 3 ? full4 : fullImageNumber === 4 ? full5 : full6} alt="FullImage" height={width > 1000 ? "70%" : width > 600 ? "90%" : "80%"} width={width > 1000 ? "70%" : width > 600 ? "90%" : "80%"} style={{borderRadius: "10px"}} />
                         <IconButton onClick={() => {nextFullImage()}}>
-                            <IoIosArrowForward style={{fontSize: width > 1000 ? "2rem" : "1.3rem", color: "black"}} />
+                            <IoIosArrowForward style={{fontSize: width > 1000 ? "2rem" : "1.3rem", color: theme === "light" ? "black" : "white", transition: "0.3s"}} />
                         </IconButton>
                     </div>
                 :
@@ -124,10 +126,10 @@ const Cutsbyjz = () => {
                         <img src={fullImageNumber === 0 ? full1 : fullImageNumber === 1 ? full2 : fullImageNumber === 2 ? full3 : fullImageNumber === 3 ? full4 : fullImageNumber === 4 ? full5 : full6} alt="MobiImage" height="100%" width="100%" style={{borderRadius: "10px"}} />
                         <div style={{marginTop: "10px"}}>
                             <IconButton onClick={() => {prevFullImage()}}>
-                                <IoIosArrowBack style={{fontSize: width > 1000 ? "2rem" : "1.5rem", color: "black"}} />
+                                <IoIosArrowBack style={{fontSize: width > 1000 ? "2rem" : "1.5rem", color: theme === "light" ? "black" : "white", transition: "0.3s"}} />
                             </IconButton>
                             <IconButton onClick={() => {nextFullImage()}}>
-                                <IoIosArrowForward style={{fontSize: width > 1000 ? "2rem" : "1.5rem", color: "black"}} />
+                                <IoIosArrowForward style={{fontSize: width > 1000 ? "2rem" : "1.5rem", color: theme === "light" ? "black" : "white", transition: "0.3s"}} />
                             </IconButton>
                         </div>
                     </div>
@@ -135,18 +137,18 @@ const Cutsbyjz = () => {
             </Grid>
 
             <Grid container sx={{marginLeft: width > 1400 ? "150px" : width > 1100 ? "120px" : width > 900 ? "100px" : width > 700 ? "70px" : "40px", marginRight: width > 1400 ? "150px" : width > 1100 ? "120px" : width > 900 ? "100px" : width > 700 ? "70px" : "40px", marginTop: "50px", display: "flex", flexDirection: "column", width: width > 1800 ? "50%" : width > 900 ? "60%" : "80%", marginBottom: width > 700 ? "90px" : "60px"}}>
-                <div style={{border: "1px solid black", marginBottom: "20px", width: "60px"}} />
-                <Typography variant="h6" sx={{fontFamily: "Nunito", fontWeight: "bold"}}>
+                <div style={{border: theme === "light" ? "1px solid black" : "1px solid white", marginBottom: "20px", width: "60px"}} />
+                <Typography variant="h6" sx={{fontFamily: "Nunito", fontWeight: "bold", color: theme === "light" ? "black" : "white", transition: "0.3s"}}>
                     Mobile Design - {mobiImageNumber + 1}/6
                 </Typography>
                 {width > 800 ? 
                     <div style={{marginTop: "20px", display: "flex", flexDirection: "row", alignItems: "center"}}>
                         <IconButton onClick={() => {prevMobiImage()}}>
-                            <IoIosArrowBack style={{fontSize: width > 1000 ? "2rem" : "1.3rem", color: "black"}} />
+                            <IoIosArrowBack style={{fontSize: width > 1000 ? "2rem" : "1.3rem", color: theme === "light" ? "black" : "white", transition: "0.3s"}} />
                         </IconButton>
                         <img src={mobiImageNumber === 0 ? mobi1 : mobiImageNumber === 1 ? mobi2 : mobiImageNumber === 2 ? mobi3 : mobiImageNumber === 3 ? mobi4 : mobiImageNumber === 4 ? mobi5 : mobi6} alt="FullImage" style={{borderRadius: "10px", height: "50vh"}} />
                         <IconButton onClick={() => {nextMobiImage()}}>
-                            <IoIosArrowForward style={{fontSize: width > 1000 ? "2rem" : "1.3rem", color: "black"}} />
+                            <IoIosArrowForward style={{fontSize: width > 1000 ? "2rem" : "1.3rem", color: theme === "light" ? "black" : "white", transition: "0.3s"}} />
                         </IconButton>
                     </div>
                 :
@@ -154,10 +156,10 @@ const Cutsbyjz = () => {
                         <img src={mobiImageNumber === 0 ? mobi1 : mobiImageNumber === 1 ? mobi2 : mobiImageNumber === 2 ? mobi3 : mobiImageNumber === 3 ? mobi4 : mobiImageNumber === 4 ? mobi5 : mobi6} alt="MobiImage" style={{borderRadius: "10px", height: "60vh"}} />
                         <div style={{marginTop: "10px"}}>
                             <IconButton onClick={() => {nextMobiImage()}}>
-                                <IoIosArrowBack style={{fontSize: width > 1000 ? "2rem" : "1.5rem", color: "black"}} />
+                                <IoIosArrowBack style={{fontSize: width > 1000 ? "2rem" : "1.5rem", color: theme === "light" ? "black" : "white", transition: "0.3s"}} />
                             </IconButton>
                             <IconButton onClick={() => {nextMobiImage()}}>
-                                <IoIosArrowForward style={{prevMobiImage: width > 1000 ? "2rem" : "1.5rem", color: "black"}} />
+                                <IoIosArrowForward style={{prevMobiImage: width > 1000 ? "2rem" : "1.5rem", color: theme === "light" ? "black" : "white", transition: "0.3s"}} />
                             </IconButton>
                         </div>
                     </div>
