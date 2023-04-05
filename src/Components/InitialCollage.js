a46e5f7646a896d3564fcab35349c82ab3fcfa04
Branden Phan <branden.phan@gmail.com>
2023-04-05 02:24:47 -0400

    added responsiveness to collage

--- src/Components/InitialCollage.js
+++ src/Components/InitialCollage.js
@@ -4,37 +4,67 @@ import Home2 from "../Images/home2.png";
 import Home3 from "../Images/home3.png";
 import Home4 from "../Images/home4.png";
 import Home5 from "../Images/home5.png";
+import { useWidth } from "../Context/WidthContext";
 
-const InitialCollage = () => (
-    <>
-        <Container style={{marginTop: "105px"}}>
-            <ImageContainer style={{width: "690px", borderRadius: "30px 0px 0px 0px"}}>
-                <img src={Home1} alt="HomeImage1" height="100%" width="100%" style={{borderRadius: "30px 0px 0px 0px"}} />
-            </ImageContainer>
+const InitialCollage = () => {
+    const { width } = useWidth();
 
-            <ImageContainer style={{width: "350px"}}>
-                <img src={Home4} alt="HomeImage4" height="100%" width="100%" />
+    return (
+        <>
+            {width > 1000 ?
+                <>
+                    <Container style={{marginTop: "105px"}}>
+                        <ImageContainer style={{width: width > 1600 ? "690px" : width > 1300 ? "590px" : "490px", borderRadius: "30px 0px 0px 0px"}}>
+                            <img src={Home1} alt="HomeImage1" height="100%" width="100%" style={{borderRadius: "30px 0px 0px 0px"}} />
+                        </ImageContainer>
 
-            </ImageContainer>
-            <ImageContainer style={{width: "350px", borderRadius: "0px 30px 0px 0px"}}>
-                <img src={Home5} alt="HomeImage5" height="100%" width="100%" style={{borderRadius: "0px 30px 0px 0px"}} />
-            </ImageContainer>
-        </Container>
+                        <ImageContainer style={{width: width > 1600 ? "350px" : width > 1300 ? "250px" : "150px"}}>
+                            <img src={Home4} alt="HomeImage4" height="100%" width="100%" />
+                        </ImageContainer>
+                        <ImageContainer style={{width: width > 1600 ? "350px" : width > 1300 ? "250px" : "150px", borderRadius: "0px 30px 0px 0px"}}>
+                            <img src={Home5} alt="HomeImage5" height="100%" width="100%" style={{borderRadius: "0px 30px 0px 0px"}} />
+                        </ImageContainer>
+                    </Container>
 
-        <Container style={{marginTop: "20px"}}>
-            <ImageContainer style={{width: "610px", borderRadius: "0px 0px 0px 30px"}}>
-                <img src={Home2} alt="HomeImage2" height="100%" width="100%" style={{borderRadius: "0px 0px 0px 30px"}} />
-            </ImageContainer>
+                    <Container style={{marginTop: "20px"}}>
+                        <ImageContainer style={{width: width > 1600 ? "610px" : width > 1300 ? "460px" : "310px", borderRadius: "0px 0px 0px 30px"}}>
+                            <img src={Home2} alt="HomeImage2" height="100%" width="100%" style={{borderRadius: "0px 0px 0px 30px"}} />
+                        </ImageContainer>
 
-            <ImageContainer style={{width: "800px", borderRadius: "0px 0px 30px 0px"}}>
-                <img src={Home3} alt="HomeImage3" height="100%" width="100%" style={{borderRadius: "0px 0px 30px 0px"}} />
-            </ImageContainer>
-        </Container>
+                        <ImageContainer style={{width: width > 1600 ? "800px" : width > 1300 ? "650px" : "500px", borderRadius: "0px 0px 30px 0px"}}>
+                            <img src={Home3} alt="HomeImage3" height="100%" width="100%" style={{borderRadius: "0px 0px 30px 0px"}} />
+                        </ImageContainer>
+                    </Container>
+                </>
+            :
+                <>
+                    <Container style={{marginTop: "105px"}}>
+                        <ImageContainer style={{width: width > 800 ? "470px" : width > 600 ? "370px" : width > 500 ? "320px" : "250px", borderRadius: "30px 0px 0px 0px"}}>
+                            <img src={Home1} alt="HomeImage1" height="100%" width="100%" style={{borderRadius: "30px 0px 0px 0px"}} />
+                        </ImageContainer>
 
-        <div id="Education" />
-    </>
+                        <ImageContainer style={{width: width > 600 ? "150px" : "100px", borderRadius: "0px 30px 0px 0px"}}>
+                            <img src={Home4} alt="HomeImage4" height="100%" width="100%" style={{borderRadius: "0px 30px 0px 0px"}} />
+                        </ImageContainer>
+                    </Container>
 
-);
+                    <Container style={{marginTop: "10px"}}>
+                        <ImageContainer style={{width: width > 800 ? "250px" : width > 600 ? "220px" : width > 500 ? "170px" : "150px", borderRadius: "0px 0px 0px 30px"}}>
+                            <img src={Home2} alt="HomeImage2" height="100%" width="100%" style={{borderRadius: "0px 0px 0px 30px"}} />
+                        </ImageContainer>
+
+                        <ImageContainer style={{width: width > 800 ? "370px" : width > 600 ? "300px" : width > 500 ? "250px" : "200px", borderRadius: "0px 0px 30px 0px"}}>
+                            <img src={Home3} alt="HomeImage3" height="100%" width="100%" style={{borderRadius: "0px 0px 30px 0px"}} />
+                        </ImageContainer>
+                    </Container>
+                </>  
+            }
+
+            <div id="Education" />
+        </>
+    )
+
+};
 
 export default InitialCollage;
 
@@ -49,4 +79,25 @@ const ImageContainer = styled.div`
     margin-right: 10px;
     height: 388px;
     border: 1px solid #C8C8C8;
+
+    @media (max-width: 1600px) {
+        height: 288px;
+    }
+
+    @media (max-width: 1300px) {
+        height: 188px;
+    }
+
+    @media (max-width: 1000px) {
+        margin-left: 5px;
+        margin-right: 5px;
+    }
+
+    @media (max-width: 600px) {
+        height: 138px;
+    }
+
+    @media (max-width: 500px) {
+        height: 120px;
+    }
 `;
--- src/Pages/home.js
+++ src/Pages/home.js
@@ -4,15 +4,23 @@ import Education from "../Components/Education";
 import Skills from "../Components/Skills";
 import Work from "../Components/Work";
 import NewFooter from "../Components/NewFooter";
+import { useWidth } from "../Context/WidthContext";
 
 const Home = () => {
+    const { width } = useWidth();
+
     return (
         <div id="Home" style={{width: "100%", overflow: "hidden", position: "absolute", top: "0", left: "0"}}>
             <div style={{marginTop: "180px", display: "flex", justifyContent: "center", flexDirection: "column"}}>
-                <p style={{fontFamily: "Nunito", fontSize: "2rem", fontWeight: "bold", margin: "0", textAlign: "center"}}>Hi I'm Branden Phan</p>
-                <p style={{fontFamily: "Nunito", fontSize: "1.4rem", fontWeight: "bold", color: "#3672FF", margin: "0", marginTop: "10px", textAlign: "center"}}>Software Developer</p>
-                <p style={{fontFamily: "Nunito", fontSize: "1.2rem", margin: "0", marginTop: "15px", fontWeight: "bold", textAlign: "center"}}>A fourth year computer science student studying at the University of Guelph</p>
+                <p style={{fontFamily: "Nunito", fontSize: width > 1400 ? "2rem" : "1.6rem", fontWeight: "bold", margin: "0", textAlign: "center"}}>Hi I'm Branden Phan</p>
+                <p style={{fontFamily: "Nunito", fontSize: width > 1400 ? "1.4rem" : "1rem", fontWeight: "bold", color: "#3672FF", margin: "0", marginTop: "10px", textAlign: "center"}}>Software Developer</p>
+                {width > 600 ?
+                    <p style={{fontFamily: "Nunito", fontSize: width > 1400 ? "1.2rem" : "0.8rem", margin: "0", marginTop: "15px", fontWeight: "bold", textAlign: "center"}}>A fourth year computer science student studying at the University of Guelph</p>
+                :
+                    <p style={{fontFamily: "Nunito", fontSize: width > 1400 ? "1.2rem" : "0.8rem", margin: "0", marginTop: "15px", fontWeight: "bold", textAlign: "center"}}>A fourth year computer science student <br />studying at the University of Guelph</p>
+                }
             </div>
+            
             <InitialCollage />
             <Education />
             <Skills />
