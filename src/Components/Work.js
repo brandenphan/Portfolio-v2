a7eb908a830acb7f4c0de8db7238e2f60b198577
Branden Phan <branden.phan@gmail.com>
2023-04-05 00:18:05 -0400

    update

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -4,26 +4,23 @@ import LotusPNG from "../Images/Work/Lotus/Lotus.png";
 import styled from "styled-components";
 
 const Work = () => (
-    <div style={{marginTop: "300px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
-        <p style={{fontFamily: "Nunito", fontWeight: "bold", fontSize: "2rem", margin: 0}}>Work</p>
+    <div style={{marginTop: "250px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+        <p style={{fontFamily: "Nunito", fontWeight: "bold", fontSize: "2rem", margin: "0"}}>Work</p>
         
-        <div style={{height: "450px", width: "80vw", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "30px"}}>
-            <div style={{height: "500px", width: "500px", display: "flex", justifyContent: "center", alignItems: "center", position: "relative"}}>
-                <div style={{height: "100px", width: "200px", border: "1px solid grey", position: "absolute", top: "0",  borderRadius: "30px"}} />
+        <div style={{marginTop: "20px", height: "450px", width: "83vw", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "0px 0px 30px 30px", border: "1px solid #C8C8C8"}}>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                 <Button>
-                    <img src={cutsbyjzPNG} alt="CutsbyjzImage" height="100%" width="100%" style={{marginLeft: "15px", marginRight: "15px", borderRadius: "0px 0px 30px 30px", position: "absolute"}} />
+                    <img src={cutsbyjzPNG} alt="CutsbyjzImage" height="100%" width="100%" style={{borderRadius: "0px 0px 0px 30px", border: "1px solid #C8C8C8"}} />
                 </Button>
             </div>
-            <div style={{height: "500px", width: "500px", display: "flex", justifyContent: "center", alignItems: "center", position: "relative"}}>
-                <div style={{height: "100px", width: "200px", border: "1px solid grey", position: "absolute", bottom: "0",  borderRadius: "30px"}} />
-                <Button2>
-                    <img src={GuelphDenPNG} alt="GuelphDentalImage" height="100%" width="100%" style={{marginLeft: "15px", marginRight: "15px", borderRadius: "30px 30px 0px 0px", position: "absolute"}} />
-                </Button2>
-            </div>
-            <div style={{height: "500px", width: "500px", display: "flex", justifyContent: "center", alignItems: "center", position: "relative"}}>
-                <div style={{height: "100px", width: "200px", border: "1px solid grey", position: "absolute", top: "0",  borderRadius: "30px"}} />
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                 <Button>
-                    <img src={LotusPNG} alt="LotusImage" height="100%" width="100%" style={{marginLeft: "15px", marginRight: "15px", borderRadius: "0px 0px 30px 30px", position: "absolute"}} />
+                    <img src={GuelphDenPNG} alt="GuelphDentalImage" height="100%" width="100%" style={{borderRadius: "0px 0px 0px 0px", border: "1px solid #C8C8C8"}} />
+                </Button>
+            </div>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <Button>
+                    <img src={LotusPNG} alt="LotusImage" height="100%" width="100%" style={{borderRadius: "0px 0px 30px 0px", border: "1px solid #C8C8C8"   }} />
                 </Button>
             </div>
         </div>
@@ -36,13 +33,14 @@ export default Work;
 const Button = styled.button`
     border: none;
     background-color: transparent;
-    height: 350px;
-    width: 450px;
+    height: 400px;
+    width: 500px;
     display: flex;
     justify-content: center;
     align-items: center;
-    position: relative;
     transition: 0.3s linear;
+    margin-left: 10px;
+    margin-right: 10px;
 
     &:hover {
         transform: translateY(-2px);
--- src/Pages/home.js
+++ src/Pages/home.js
@@ -9,7 +9,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
-import { IconButton } from "@mui/material";
+import { Tooltip } from "@mui/material";
 import styled from "styled-components";
 
 // import styled from "styled-components";
@@ -35,23 +35,31 @@ const Home = () => {
             <Work />
 
     
-            <div style={{marginTop: "500px", width: "100vw", display: "flex", justifyContent: "center"}}>
+            <div style={{marginTop: "400px", width: "100vw", display: "flex", justifyContent: "center"}}>
                 <div style={{height: "400px", width: "70vw", borderRadius: "30px 30px 0px 0px", backgroundColor: "#3F3F3F", display: "flex", justifyContent: "center"}}>
                     <div>
                         <p style={{fontFamily: "Nunito", fontWeight: "bold", fontSize: "1.6rem", color: "white", marginTop: "65px", textAlign: "center"}}>Contact</p>
                         <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                            <Button>
-                                <GitHubIcon sx={{color: "white", height: "25px", width: "25px"}} />
-                            </Button>
-                            <Button>
-                                <EmailIcon sx={{color: "white", height: "25px", width: "25px"}} />
-                            </Button>
-                            <Button>
-                                <LinkedInIcon sx={{color: "white", height: "25px", width: "25px"}} />
-                            </Button>
-                            <Button>
-                                <HistoryEduIcon sx={{color: "white", height: "25px", width: "25px"}} />
-                            </Button>
+                            <Tooltip title="Github" placement="top" componentsProps={{tooltip: {sx: {backgroundColor: "white", color: "black", border: "1px solid #D7D6D6", fontFamily: "Source Sans Pro", fontWeight: "bold", fontSize: "20px"}}}}>
+                                <Button>
+                                    <GitHubIcon sx={{color: "white", height: "25px", width: "25px"}} />
+                                </Button>
+                            </Tooltip>
+                            <Tooltip title="Email" placement="top" componentsProps={{tooltip: {sx: {backgroundColor: "white", color: "black", border: "1px solid #D7D6D6", fontFamily: "Source Sans Pro", fontWeight: "bold", fontSize: "20px"}}}}>
+                                <Button>
+                                    <EmailIcon sx={{color: "white", height: "25px", width: "25px"}} />
+                                </Button>
+                            </Tooltip>
+                            <Tooltip title="LinkedIn" placement="top" componentsProps={{tooltip: {sx: {backgroundColor: "white", color: "black", border: "1px solid #D7D6D6", fontFamily: "Source Sans Pro", fontWeight: "bold", fontSize: "20px"}}}}>
+                                <Button>
+                                    <LinkedInIcon sx={{color: "white", height: "25px", width: "25px"}} />
+                                </Button>
+                            </Tooltip>
+                            <Tooltip title="Resume" placement="top" componentsProps={{tooltip: {sx: {backgroundColor: "white", color: "black", border: "1px solid #D7D6D6", fontFamily: "Source Sans Pro", fontWeight: "bold", fontSize: "20px"}}}}>
+                                <Button>
+                                    <HistoryEduIcon sx={{color: "white", height: "25px", width: "25px"}} />
+                                </Button>
+                            </Tooltip>
                         </div>
                         <p style={{fontFamily: "Nunito", fontWeight: "bold", fontSize: "1rem", color: "white", marginTop: "110px", textAlign: "center"}}>Designed by Branden Phan</p>
                         <p style={{fontFamily: "Nunito", fontWeight: "bold", fontSize: "1rem", color: "white", textAlign: "center"}}>Copyright © 2022 - All Rights Reserved.</p>
@@ -86,6 +94,9 @@ const Button = styled.button`
     margin-left: 10px;
     margin-right: 10px;
     transition: 0.2s linear;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     &:hover {
         background-color: #5C8DFF;
