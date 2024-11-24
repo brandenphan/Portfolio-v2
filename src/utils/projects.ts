24db6810036725203e30e20e1e826b091c96db2a
Branden Phan <branden.phan@gmail.com>
2024-11-24 01:52:57 -0500

    Updated text

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,5 +1,5 @@
 export const projects = [
-  { name: "Recapify", desc: "Google Chrome extension that allows users to quickly summarize highlighted text using GPT-3.5 with OpenAI's LangChain API.", link: "https://github.com/L-Michael1/Recapify", languages: ["JavaScript", "Python", "HTML", "CSS"] },
+  { name: "Recapify", desc: "Google Chrome extension that allows users to quickly summarize highlighted text.", link: "https://github.com/L-Michael1/Recapify", languages: ["JavaScript", "Python", "HTML", "CSS"] },
   { name: "Course Search", desc: "React web app that gives users the ability to search courses, degrees, and prerequisite trees from the University of Guelph and British Columbia.", link: "https://github.com/brandenphan/Course-Search", languages: ["JavaScript", "HTML", "CSS"] },
   { name: "Schedule Maker", desc: "Full-stack web app that can create unique schedules, saved under personal user accounts.", link: "https://github.com/brandenphan/Schedule-Maker", languages: ["JavaScript", "HTML", "CSS"] },
   { name: "Discord Bot", desc: "Discord bot featuring various features such as, Google Searching, Trivia, and To do lists.", link: "https://github.com/brandenphan/Pami-Bot", languages: ["Python"] }
